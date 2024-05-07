e9b1d68dca3ed99984a5532fd13bd883dc8d6af2
Jussif79 <jussif.79@gmail.com>
2024-05-07 15:31:00 +0200

    Added hotel_db sql code

---
+++ hotel_db.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS hotel_db;
+
+SET
+    SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+USE hotel_db;
+
+CREATE TABLE admins(
+    id VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+INSERT INTO
+    admins (id, name, password)
+VALUES
+    (
+        "EQYJaB96HcaTtxag6J7d",
+        "admin",
+        "6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2"
+    );
+
+CREATE TABLE bookings (
+    user_id VARCHAR(20) NOT NULL,
+    booking_id VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    rooms INT(1) NOT NULL,
+    check_in VARCHAR(10) NOT NULL,
+    check_out VARCHAR(10) NOT NULL,
+    adults INT(1) NOT NULL,
+    childs INT(1) NOT NULL
+);
+
+CREATE TABLE messages (
+    id VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    message VARCHAR(1000) NOT NULL
+);
