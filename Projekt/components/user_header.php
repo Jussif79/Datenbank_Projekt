b8bc1799727bb69d92f7d24448cd03abfa17bb58
Jussif79 <150897455+Jussif79@users.noreply.github.com>
2024-05-21 15:49:16 +0200

    Update user_header.php

--- Projekt/components/user_header.php
+++ Projekt/components/user_header.php
@@ -3,19 +3,19 @@
 <section class="header">
 
    <div class="flex">
-      <a href="#home" class="logo">Hotels And Resorts</a>
-      <a href="#availability" class="btn">check availability</a>
+      <a href="#home" class="logo">BFO Hotel</a>
+      <a href="#availability" class="btn">Verfügbarkeit prüfen</a>
       <div id="menu-btn" class="fas fa-bars"></div>
    </div>
 
    <nav class="navbar">
-      <a href="index.php#home">Home</a>
-      <a href="index.php#about">About</a>
-      <a href="index.php#reservation">Reservation</a>
-      <a href="index.php#gallery">Gallery</a>
-      <a href="index.php#contact">Contact</a>
-      <a href="index.php#reviews">Reviews</a>
-      <a href="bookings.php">My Bookings</a>
+      <a href="index.php#home">Hauptseite</a>
+      <a href="index.php#about">Über uns</a>
+      <a href="index.php#reservation">Reservierung</a>
+      <a href="index.php#gallery">Galerie</a>
+      <a href="index.php#contact">Kontakt</a>
+      <a href="index.php#reviews">Bewertungen</a>
+      <a href="bookings.php">Meine Reservierungen</a>
    </nav>
 
 </section>
